e085075b1318a8e27faa8234cd59d834992bd63c
RotemLibrati <rotemlibrati1@gmail.com>
2022-04-11 19:20:41 +0300

    Add a new navigation screen

--- App.js
+++ App.js
@@ -5,15 +5,17 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MainScreen from './screens/MainScreen';
 import LoginScreen from './screens/LoginScreen';
 import SingupScreen from './screens/SingupScreen';
+import NoteScreen from './screens/NoteScreen';
 
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} options={{headerShown: false}} />
         <Stack.Screen name="SignupScreen" component={SingupScreen} options={{ title: 'Create New User'}} />
         <Stack.Screen name="MainScreen" component={MainScreen} options={{ title: 'Main'}}/>
+        <Stack.Screen name="NoteScreen" component={NoteScreen} options={{ title: 'Create New Note'}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
