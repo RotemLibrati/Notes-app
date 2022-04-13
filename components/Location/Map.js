8365f29a5ba8e13dd0bf34b7ad3f3443f14ff534
RotemLibrati <rotemlibrati1@gmail.com>
2022-04-13 08:14:33 +0300

    Displays the map and pins on the map according to locations

---
+++ components/Location/Map.js
@@ -0,0 +1,70 @@
+import { StyleSheet, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import MapView from 'react-native-maps';
+import { auth } from '../../firebase';
+import * as firebase from '../../firebase';
+const Map = () => {
+    const [notes, setNotes] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        firebase.getNotesByEmail("notes", auth.currentUser?.email)
+            .then(function (notesList) {
+                setNotes(firebase.getSortedArrayDateFromDict(notesList));
+                setIsLoading(false);
+            })
+            .catch(error => alert(error.message));
+    }, [notes]);
+    let lat, lng;
+    try {
+        lat = notes[0].location.lat;
+        lng = notes[0].location.lng;
+    } catch (e) {
+        lat = 32.109333;
+        lng = 34.855499;
+    }
+    return (
+        <View style={styles.container}>
+            <MapView
+                style={styles.map}
+                region={{
+                    latitude: lat,
+                    longitude: lng,
+                    latitudeDelta: 0.5,
+                    longitudeDelta: 0.5
+                }}>
+                {isLoading ? <></> :
+                    notes.map((note) => (
+                        <MapView.Marker
+                            coordinate={{
+                                latitude: note.location.lat,
+                                longitude: note.location.lng
+                            }}
+                            title={note.title}
+                            description={note.body}
+                        />))}
+            </MapView>
+        </View >
+    )
+};
+
+export default Map;
+
+const styles = StyleSheet.create({
+    container: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        justifyContent: 'flex-end',
+        alignItems: 'center'
+    },
+    map: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+    }
+});
