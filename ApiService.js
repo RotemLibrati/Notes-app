21e90b4018913141bca0c60d60805217a78f7c6f
RotemLibrati <rotemlibrati1@gmail.com>
2022-04-17 13:15:26 +0300

    Api key for firebase

---
+++ ApiService.js
@@ -0,0 +1,10 @@
+export default {
+    firebaseConfig : {
+        apiKey: "AIzaSyBB5xAtFrcfTzQBtTA2bw78PgCKVKHw-Cw",
+        authDomain: "notes-project-8f779.firebaseapp.com",
+        projectId: "notes-project-8f779",
+        storageBucket: "notes-project-8f779.appspot.com",
+        messagingSenderId: "638286220396",
+        appId: "1:638286220396:web:45183f9cf288e285895c49"
+      }
+}
